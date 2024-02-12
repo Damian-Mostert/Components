c54eeff6f03e306edfc5b35afea6f81a33c5459d
Damian Mostert <damianmostert86@gmail.com>
2024-02-12 12:32:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export { default as Button } from "./button/button"
 export { default as Nav } from "./nav/nav"
 export { default as Accordion } from "./accordion/accordion"
 export { default as Popup } from "./popup/popup"
+export {default as Layout} from "./layout/layout"
 //menu edit build is used in popup.
 export {
     default as JsonEdit,
