e040ede705274409045f07109abd33f892a90c97
Damian Mostert <damianmostert86@gmail.com>
2024-02-12 12:27:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 //general components
 export { default as Button } from "./button/button"
 export { default as Nav } from "./nav/nav"
-export { default as Header } from "./navigation/header"
 export { default as Accordion } from "./accordion/accordion"
 export { default as Popup } from "./popup/popup"
 //menu edit build is used in popup.
